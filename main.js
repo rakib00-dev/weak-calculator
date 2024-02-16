8f69104f7826f2fe60c45e020b39cda4225e5f8c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-16 17:36:59 +0600

    added all number key in keyboard

--- main.js
+++ main.js
@@ -23,7 +23,37 @@ function calculate() {
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case '8':
-      display.value = '8';
+      display.value += '8';
+      break;
+    case '9':
+      display.value = '9';
+      break;
+    case '0':
+      display.value = '0';
+      break;
+    case '7':
+      display.value = '7';
+      break;
+    case '6':
+      display.value = '6';
+      break;
+    case '5':
+      display.value = '5';
+      break;
+    case '4':
+      display.value = '4';
+      break;
+    case '3':
+      display.value = '3';
+      break;
+    case '2':
+      display.value = '2';
+      break;
+    case '1':
+      display.value = '1';
+      break;
+    case '.':
+      display.value = '.';
       break;
 
     default:
